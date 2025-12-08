26a1c367bc9ef92396c2a80c2d395ff022e71361
MohitPimoli <mohitpimoli31@gmail.com>
2025-12-08 22:37:10 +0530

    Add: button component and Auth page

---
+++ lens/src/page/AuthPage.jsx
@@ -0,0 +1,13 @@
+import { Button } from "../components/common/Buttton";
+import form from "../config";
+
+export const AuthPage = () => {
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+  const handleChange = () => {};
+  return (
+    <>
+      {form.filter(() => {})}
+      <Button text={"Login"} onClick={handleChange} />
+    </>
+  );
+};
